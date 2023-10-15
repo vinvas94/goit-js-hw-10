65f17eccf4ec4670168bb7b18f5b50c249814486
vinvas94 <vintoniv1111@gmail.com>
2023-10-15 13:00:12 +0200

    add loader

--- src/index.js
+++ src/index.js
@@ -1,48 +1,13 @@
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
-
+import 'slim-select/dist/slimselect.css';
 import { fetchBreeds, fetchCatByBreeds } from './cat-api';
+
 const refs = {
   select: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
-  error: document.querySelector('.error'),
   list: document.querySelector('.cat-info'),
 };
-
-refs.select.style = 'width: 1200px';
-refs.select.addEventListener('change', onChangeOption);
-refs.error.classList.add('hide');
-refs.loader.classList.remove('hide');
-
-function onChangeOption(event) {
-  fetchCatByBreeds(event.target.value)
-    .then(({ data }) => {
-      let { name, description, temperament } = data[0].breeds[0];
-      let { url } = data[0];
-      refs.list.innerHTML = catMarkup({
-        name,
-        description,
-        temperament,
-        url,
-      });
-    })
-    .catch(() =>
-      Notiflix.Notify.failure('FAILED FETCH API', {
-        position: 'right-top',
-      })
-    )
-    .finally(refs.loader.classList.add('hide'));
-}
-
-function catMarkup({ name, description, temperament, url }) {
-  return `
-    <img src=${url} alt="${name}" style="width: 1200px" />
-    <div style="width: 1200px">
-    <h2>${name}</h2>
-    <p>${description}</p>
-    <p><span>Temperament:</span> ${temperament}</p>
-    </div >`;
-}
 
 function populateBreedOptions(data) {
   return data
@@ -52,24 +17,62 @@ function populateBreedOptions(data) {
 
 function loadBreeds() {
   refs.loader.classList.remove('hide');
-  return fetchBreeds()
+  fetchBreeds()
     .then(({ data }) => {
-      refs.loader.classList.add('hide');
       refs.select.innerHTML = populateBreedOptions(data);
       new SlimSelect({
         select: refs.select,
-        settings: {},
       });
     })
-    .catch(() =>
+    .catch(error => {
       Notiflix.Notify.failure(
         'Oops! Something went wrong! Try reloading the page!',
         {
           position: 'right-top',
         }
-      )
-    )
-    .finally(refs.loader.classList.add('hide'));
+      );
+    })
+    .finally(() => {
+      refs.loader.classList.add('hide');
+      refs.select.classList.remove('hide');
+    });
 }
 
+refs.select.addEventListener('change', onChangeOption);
 loadBreeds();
+
+function onChangeOption(event) {
+  refs.loader.classList.remove('hide');
+  fetchCatByBreeds(event.target.value)
+    .then(({ data }) => {
+      refs.list.innerHTML = '';
+      refs.loader.classList.remove('hide');
+      let { name, description, temperament } = data[0].breeds[0];
+      let { url } = data[0];
+      refs.list.innerHTML = catMarkup({
+        name,
+        description,
+        temperament,
+        url,
+      });
+    })
+    .catch(error => {
+      refs.list.innerHTML = '';
+      Notiflix.Notify.failure('FAILED FETCH API', {
+        position: 'right-top',
+      });
+    })
+    .finally(() => {
+      refs.loader.classList.add('hide');
+    });
+}
+
+function catMarkup({ name, description, temperament, url }) {
+  return `
+    <img src="${url}" alt="${name}" style="width: 1240px" />
+    <div style="width: 1240px">
+    <h2>${name}</h2>
+    <p>${description}</p>
+    <p><span>Temperament:</span> ${temperament}</p>
+    </div >`;
+}
